58b1b363aeb0df5c905784e94fc9b0cf907f3469
Valentin Deleplace <deleplace2015@gmail.com>
2024-05-07 13:04:13 +0200

    equalsums using math/rand/v2 and a ChaCha8 source.

--- equalsums_test.go
+++ equalsums_test.go
@@ -1,13 +1,16 @@
 package equalsums
 
 import (
+	"bytes"
 	"math/rand/v2"
 	"testing"
 )
 
 func testSolve(t testing.TB) {
 	numbers := []int{738, 408, 588, 559, 976, 120, 713, 95, 620}
-	r := rand.New(rand.NewPCG(42, 42))
+	var seed [32]byte
+	copy(seed[:], bytes.Repeat(nil, 42))
+	r := rand.New(rand.NewChaCha8(seed))
 	sub1, sub2 := Solve(numbers, r)
 
 	if eq(sub1, sub2) {
