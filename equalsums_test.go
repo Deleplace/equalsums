503db869d960667e3d7af0d8c0414cd070d67371
Valentin Deleplace <deleplace2015@gmail.com>
2024-05-14 10:51:21 +0200

    equalsums using 10 seeds to compare different RNGs' average perf.

--- equalsums_test.go
+++ equalsums_test.go
@@ -27,25 +27,38 @@ func TestSolve(t *testing.T) {
 
 func BenchmarkSolveRand(b *testing.B) {
 	for range b.N {
-		r := rand.New(rand.NewSource(42))
-		testSolve(b, r)
+		// Perf numbers for the "Equal Sums" algo depend a lot on the specific sequence
+		// produced by the RNG.
+		// To average out and make comparisons more meaningful, we use 10 different seeds.
+		for s := int64(42); s < 52; s++ {
+			r := rand.New(rand.NewSource(s))
+			testSolve(b, r)
+		}
 	}
 }
 
 func BenchmarkSolveChaCha8(b *testing.B) {
 	for range b.N {
-		var seed [32]byte
-		copy(seed[:], bytes.Repeat([]byte{42}, 32))
-		r := randv2.New(randv2.NewChaCha8(seed))
-		testSolve(b, r)
+		// Perf numbers for the "Equal Sums" algo depend a lot on the specific sequence
+		// produced by the RNG.
+		// To average out and make comparisons more meaningful, we use 10 different seeds.
+		for s := byte(42); s < 52; s++ {
+			var seed [32]byte
+			copy(seed[:], bytes.Repeat([]byte{s}, 32))
+			r := randv2.New(randv2.NewChaCha8(seed))
+			testSolve(b, r)
+		}
 	}
 }
 
 func BenchmarkSolvePCG(b *testing.B) {
 	for range b.N {
-		var seed [32]byte
-		copy(seed[:], bytes.Repeat([]byte{42}, 32))
-		r := randv2.New(randv2.NewPCG(42, 42))
-		testSolve(b, r)
+		// Perf numbers for the "Equal Sums" algo depend a lot on the specific sequence
+		// produced by the RNG.
+		// To average out and make comparisons more meaningful, we use 10 different seeds.
+		for s := uint64(42); s < 52; s++ {
+			r := randv2.New(randv2.NewPCG(s, s))
+			testSolve(b, r)
+		}
 	}
 }
